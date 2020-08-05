dcebf441e0ac0b64d08fa896f87aecc8882d0aab
Mojjammil Khandker <mojjammil@gmail.com>
2020-08-05 15:23:37 +0300

    Final code after lesson 2 tutorial

--- react/hacker-stories/src/App.js
+++ react/hacker-stories/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const title = 'React'
+// const title = 'React'
 // const welcome = {
 //   greeting: 'Hey',
 //   title: 'React'
@@ -12,44 +11,87 @@ const title = 'React'
 //   return title
 // }
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState)
 
-const List = () => {
-  return list.map(item => {
-    return( 
-    <div key={item.objectID}>
-     <span>
+  React.useEffect(() => {
+    localStorage.setItem(key, value)
+  }, [value, key])
+
+  return [value, setValue]
+}
+
+const List = ({list}) =>
+  list.map(item => <Item key={item.objectID} item={item} />)
+
+const Item = ({item}) => (
+  <div>
+    <span>
        <a href={item.url}>{item.title}</a>
      </span>
      <span>{item.author}</span>
      <span>{item.num_comments}</span>
      <span>{item.points}</span>
-    </div>)
-    })
-}
+  </div>
+)
 
+// const Search = props => {
+//   const {search, onSearch} = props
+//   return (
+//     <div>
+//       <label htmlFor="search">Search: </label>
+//       <input 
+//       id="search" 
+//       type="text" 
+//       value={search}
+//       onChange={onSearch}/>
+//     </div>
+//   )}
+
+  const Search = ({search, onSearch}) => (
+    <div>
+        <label htmlFor="search">Search: </label>
+        <input 
+        id="search" 
+        type="text" 
+        value={search}
+        onChange={onSearch}/>
+      </div>
+  )
 
 const App = () => {
-  const handleChange = event => {
-    console.log(event.target.value)
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    'search', 
+    'React'
+  )
+  
+  const handleSearch = event => {
+    setSearchTerm(event.target.value)
   }
+
+  const searchedStories = stories.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase()))
+  
   return (
 
     // do something in between 
@@ -59,10 +101,10 @@ const App = () => {
        {/* Hello {getTitle('React')} */}
        My Hacker Stories
       </h1>
-
+      <Search search={searchTerm} onSearch={handleSearch}/>
       <hr />
 
-      <List />
+      <List list={searchedStories}/>
 
       {/* Created a separate list component */}
       {/* {list.map(function(item) {
@@ -76,9 +118,6 @@ const App = () => {
          <span>{item.points}</span>
         </div>)
       })} */}
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange}/>
     </div>
   );
 }
