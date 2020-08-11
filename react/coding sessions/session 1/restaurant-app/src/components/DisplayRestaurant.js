5b373240d7cbbddf29fd3e11dad3aeae21040218
mostafaIn <mostafa2015se@gmail.com>
2020-08-11 15:55:22 +0300

    coding session 4: filter out the favorite items, make a nice spinner and use localstorage to keep the favorite ones

--- react/coding sessions/session 1/restaurant-app/src/components/DisplayRestaurant.js
+++ react/coding sessions/session 1/restaurant-app/src/components/DisplayRestaurant.js
@@ -6,7 +6,7 @@ const DisplayRestaurant = (props) => {
   const [fav, setFav] = useState(false)
   // console.log('restaurants from display component ', props.restaurantList.businesses)
   const toggleFav = (restaurant) => {
-    const targetRestaurant = props.restaurantList.businesses.find(res => res.id === restaurant.id)
+    const targetRestaurant = props.restaurantList.find(res => res.id === restaurant.id)
     console.log(targetRestaurant)
     if(targetRestaurant.id === restaurant.id){
       setFav(!fav)
@@ -16,18 +16,22 @@ const DisplayRestaurant = (props) => {
     }else{
       restaurant.favorite = false
     }
+    localStorage.setItem('restaurantList', JSON.stringify(props.restaurantList))
   }
   
   return (
     <div className="restaurants">
-      {props.restaurantList.businesses.map((restaurant, key) => {
+      {props.restaurantList.map((restaurant, key) => {
         return (
           <div key={key} className="cards">
+            <div onClick={() => toggleFav(restaurant)}>
             {(restaurant.favorite) ?
-            <i onClick={() => toggleFav(restaurant)} className="fas fa-heart"></i>
+            <i className="fas fa-heart"></i>
             :
-            <i onClick={() => toggleFav(restaurant)} className="far fa-heart"></i>
+            <i className="far fa-heart"></i>
             }
+            </div>
+            
           <a href={restaurant.url} target="blank">
             <img src={restaurant.image_url} />
             <p className="restaurantName"> {restaurant.name}</p>
