d0edc3b77fd88bf3130816696871b968cb11af1b
mostafaIn <mostafa2015se@gmail.com>
2020-08-19 16:06:59 +0300

    simple registration form version 0.1

---
+++ react/coding sessions/session 4/registration-form/src/components/constants/regExp.js
@@ -0,0 +1,3 @@
+export const nameRex = RegExp(/^[a-zA-Z\s]{2,15}$/);
+export const emailRex = RegExp(/^\w+([-]?\w+)*@\w+([-]\w+)*(\.\w{2,3})$/)
+export const phoneRex = RegExp(/^([+358][4|5][0-9]{1})[-\s]?([0-9]{7})$/)
